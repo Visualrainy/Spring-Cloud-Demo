d4871d4965838f62db41874d1dbb15c52a77e0f2
Peng <visualrainy@gmail.com>
2019-05-31 10:03:30 +0800

    Add config server module

--- eureka-server/src/main/java/com/spring/cloud/eureka/DiscoverApplication.java
+++ eureka-server/src/main/java/com/spring/cloud/eureka/DiscoverApplication.java
@@ -1,4 +1,4 @@
-package com.spring.cloud.eureka.server;
+package com.spring.cloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
