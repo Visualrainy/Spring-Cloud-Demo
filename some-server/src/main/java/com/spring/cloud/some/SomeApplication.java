0ec749cd82e94fcb3ccaf544132547d82cdc66bb
Peng <visualrainy@gmail.com>
2019-05-31 10:41:32 +0800

    Add new module some server as second server

---
+++ some-server/src/main/java/com/spring/cloud/some/SomeApplication.java
@@ -0,0 +1,26 @@
+package com.spring.cloud.some;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class SomeApplication {
+    //通过@Value注入来自config server的值
+    @Value("${my.message}")
+    private String message;
+
+    @RequestMapping(value = "/getsome")
+    public String getSome() {
+        return message;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SomeApplication.class, args);
+    }
+}
