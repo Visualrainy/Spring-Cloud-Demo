173d795e474177e025ba557f1037d457d8535430
Peng <visualrainy@gmail.com>
2019-05-31 14:02:41 +0800

    Add comment @EnableDiscoveryClient is similar with @EnableEurekaClient, and fix typo error

--- some-server/src/main/java/com/spring/cloud/some/SomeApplication.java
+++ some-server/src/main/java/com/spring/cloud/some/SomeApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableDiscoveryClient //similar as @EnableEurekaClient
 @RestController
 public class SomeApplication {
     //通过@Value注入来自config server的值
